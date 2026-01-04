1be428725d9df147666b904a57aadfb71e178403
LindaCamacho <ceo130506@gmail.com>
2026-01-03 22:03:23 -0600

    Actualizacion settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'proyecto_simulacion', 
+    'data_analysis',
     'rest_framework',
     'corsheaders',
 ]
